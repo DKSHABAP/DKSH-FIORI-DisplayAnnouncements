169adad7b835a167de92c62b8a23cb4638864d83
DKSHABAP <KUL_CSSC_GTD_ABAP@dksh.com>
2022-03-30 08:37:18 +0000

    test

--- Component.js
+++ Component.js
@@ -30,8 +30,6 @@ sap.ui.define([
 		},
 		onOpenPopover: function (oEvent) {
 			var oButton = oEvent.getSource();
-			// oView = this.getView();
-			// create popover
 			if (!this._pPopover) {
 				this._pPopover = Fragment.load({
 					// id: oView.getId(),
